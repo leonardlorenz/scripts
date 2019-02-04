6ee19bf973d0c822386e52379640dafa1c8adb5c
Leonard Lorenz <contact@metr1xx.de>
2019-02-04 14:55:12 +0100

    updated sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,26 +1,117 @@
-#/bin/bash
+#!/bin/bash
 
-# CPU usage
-# https://stackoverflow.com/questions/9229333/how-to-get-overall-cpu-sage-e-g-57-on-linux
-# author: netcoder
-# modified
-top -bn1 | grep "Cpu(s)" | \
-           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-           awk '{print 100 - $1"%"}' | \
-           sed "s/^/CPU%:\ /g"
+isDesktop="$(acpi 2>&1 | grep power_supply)"
+
+icon() {
+    echo -e "%{T2}\u$1%{T-}%{T1}"
+}
+
+# Show Time
+dateD() {
+    day=$(date +"%d.%m.%Y")
+    echo "$day"
+}
+
+dateC() {
+    clock=$(date +"%I:%M%P")
+    echo "$clock"
+}
+
+# Battery Percentage
+battery() {
+    if [ -n "$isDesktop" ]
+    then
+        echo "BAT: Power Supply"
+    else
+        PLUGGED=$(acpi -a | tail -c 8 | grep 'on-line')
+        if [ "$PLUGGED" ]
+	    then
+            charging=" (charging)"
+	    else
+	    	charging=""
+	    fi
+        batperc="$(acpi --battery | awk -F ',' '{print $2}')"
+        echo "BAT: $batperc$charging"
+    fi
+}
+
+# CPU Usage
+cpuP() {
+    cpuperc=$(top -bn1 | grep "Cpu(s)" | \
+       sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+       awk '{print 100 - $1"%"}')
+    echo "CPU: $cpuperc"
+}
 
 # CPU temperature
-# check `sensors` output for the right grep identifier. your temp sensor might not be called Tdie
-sensors | grep Tdie | awk '{ print $2 }' | sed 's/\+/CPUT:\ /g'
-
-# GPU usage
-# check `sensors` output for the right grep identifier. your temp sensor might not be called temp1
-sensors | grep temp1 | awk '{ print $2 }' | sed 's/\+/GPUT:\ /g'
+cpuT() {
+    if [ -n "$isDesktop" ]
+    then
+        cputemp=$(sensors | grep "Tdie" | awk '{ print $2 }' | sed 's/\+/CPUT:\ /g')
+        echo "$cputemp"
+    else
+        cputemp=$(sensors | grep "Core 0" | awk '{ print $3 }' | sed 's/\+/CPUT:\ /g')
+        echo "$cputemp"
+    fi
+}
 
 # GPU temperature
-# yet to be found
+gpuT() {
+    gputemp=$(sensors | grep temp1 | awk '{ print $2 }' | sed 's/\+/GPUT:\ /g')
+    echo "$gputemp"
+}
 
-# output should look something like this
-# CPU%: 2.7%
-# CPUT: 36.0°C
-# GPUT: 36.0°C
+updates() {
+    updates=$(checkupdates | wc -l)
+
+    if [ "$updates" -eq 1 ]; then
+        echo "$updates update"
+    elif [ "$updates" -gt 1 ]; then
+        echo "$updates updates"
+    else
+        echo "no updates"
+    fi
+}
+
+network() {
+    network=$(iwgetid --scheme)
+    if [ -z "$network" ]
+    then
+        echo "Wired"
+    else
+        echo "$network"
+    fi
+}
+
+memory() {
+    memory=$(free | grep Mem | awk '{print int($3/$2 * 100.0)}')
+    echo "RAM: $memory%"
+}
+
+keyboard() {
+    layout=$(setxkbmap -query | grep --color=never layout | awk '{ print $2}')
+    variant=$(setxkbmap -query | grep --color=never variant | awk '{ print $2}')
+    echo "$layout($variant)"
+}
+
+i3() {
+    workspaces=$(~/Scripts/lemonbar/ws.sh)
+    echo "%{A4:i3-msg workspace next:}%{A5:i3-msg workspace prev:} $workspaces%{A}%{A}"
+}
+
+monitors() {
+    monitors=$(xrandr | grep -o "^.* connected" | sed "s/ connected//")
+    echo "$monitors"
+}
+
+notify-send "\
+$(dateD)
+$(dateC)
+$(cpuP)
+$(memory)
+$(cpuT)
+$(gpuT)
+$(network)
+$(battery)
+$(updates)
+$(keyboard)"
